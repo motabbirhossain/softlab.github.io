954f0fde15979e47e6f2f0438dfef1da60031afc
RajuAhmed2 <92273686+RajuAhmed2@users.noreply.github.com>
2023-02-12 19:22:15 +0600

    first commit

---
+++ assets/js/active.js
@@ -0,0 +1,62 @@
+(function($) {
+    "use strict";
+
+    $(document).ready(function () {
+
+        $('#hamburger').on('click', function() {            
+            $('.mobile-nav').addClass('show');
+            $('.overlay').addClass('active');
+        });
+
+        $('.close-nav').on('click', function() {            
+            $('.mobile-nav').removeClass('show');            
+            $('.overlay').removeClass('active');          
+        });
+
+        $(".overlay").on("click", function () {
+            $(".mobile-nav").removeClass("show");
+            $('.overlay').removeClass('active');
+        });
+
+        $("#mobile-menu").metisMenu();
+
+
+        /* =============================================
+            # owlCarousel init
+        ===============================================*/
+
+        $(".testimonial_widget").owlCarousel({        
+            items: 1,     
+            dots: true,
+            loop: true,
+            autoplayTimeout: 2000,
+            autoplay:true,
+            nav: true,       
+            navText: ['<i class="far fa-angle-right"></i>', '<i class="far fa-angle-left"></i>'],
+           
+        });
+
+        /*==========================
+           Scroll To Up Init
+        ============================*/
+        $.scrollUp({
+            scrollName: 'scrollUp',
+            topDistance: '1110',
+            topSpeed: 2000,
+            animation: 'slide',
+            animationInSpeed: 300,
+            animationOutSpeed: 300,
+            scrollText: '<i class="fal fa-angle-up"></i>',
+            activeOverlay: false, 
+        });
+
+        new WOW().init();
+
+        $('.counter').counterUp({
+            delay: 10,
+            time: 3000
+        });
+        
+    }); // end document ready function
+
+})(jQuery); // End jQuery
